9b189974afec47b3b5d2ba66ebc75edce23f4c7b
Nikita Klimoyts <everygreen22@yandex.ru>
2022-12-13 18:17:44 +0300

    get_queryset fixed

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -44,7 +44,7 @@ class FollowViewSet(mixins.CreateModelMixin,
     search_fields = ('user__username', 'following__username')
 
     def get_queryset(self):
-        return self.request.user.following.all()
+        return Follow.objects.filter(following=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
